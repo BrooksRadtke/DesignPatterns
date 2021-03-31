765c865c6b38b24b889bfb04caa00ca04965725a
Brooks Radtke <brooks@brooksradtke.com>
2021-03-31 10:12:03 -0700

    Push Builder Pattern to Repository

---
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Client code creates a build object, passes it to the director,
+            //then initiates the construction process. End result is retrieved
+            //from the builder object.
+
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+            director.Builder = builder;
+
+            Console.WriteLine("Standard basic product: ");
+            director.BuildMinimalVialbleProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Standard full featured product: ");
+            director.BuildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            //Builder patterns can also be used without a Director class
+            Console.WriteLine("Custom product: ");
+            builder.BuildPartA();
+            builder.BuildPartC();
+            Console.WriteLine(builder.GetProduct().ListParts());
+            Console.ReadLine();
+        }
+    }
+
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product _product = new Product();
+
+        public ConcreteBuilder()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this._product = new Product();
+        }
+
+        // All production steps work with the same product instance
+        public void BuildPartA()
+        {
+            this._product.Add("PartA1");
+        }
+
+        public void BuildPartB()
+        {
+            this._product.Add("PartB1");
+        }
+
+        public void BuildPartC()
+        {
+            this._product.Add("PartC1");
+        }
+
+        public Product GetProduct()
+        {
+            Product result = this._product;
+            this.Reset();
+            return result;
+        }
+    }
+
+    public class Product
+    {
+        private List<object> _parts = new List<object>();
+
+        public void Add(string part)
+        {
+            this._parts.Add(part);
+        }
+
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for(int i = 0; i < this._parts.Count; i++)
+            {
+                str += this._parts[i] + ", ";
+            }
+
+            // Removes the last ",c"
+            str = str.Remove(str.Length - 2);
+
+            return "Product parts: " + str + "\n";
+        }
+    }
+
+    public class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+
+        public void BuildMinimalVialbleProduct()
+        {
+            this._builder.BuildPartA();
+        }
+
+        public void BuildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+    }
+}
